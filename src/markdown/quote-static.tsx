d20391bb9afc60c6933fcfe0afbc49eb7aa7c0f8
Kamil Gajowy <kamil.gajowy@gmail.com>
2018-09-28 21:45:46 +0200

    chore: remove redundant div

--- src/markdown/quote-static.tsx
+++ src/markdown/quote-static.tsx
@@ -3,21 +3,17 @@ import { renderToStaticMarkup } from 'react-dom/server'
 import styled from 'styled-components'
 import { Quote } from '../ui/Quote'
 
-const QuoteContainer = styled.div`
+const QuoteContainer = styled(Quote)`
   width: 50%;
   text-align: center;
   margin: 0 auto;
 `
+
 export const quoteStatic = (quote: string) => {
     /**
      *  if 'quote' contains content being relatively HTML in format: <p>Quote</p>
      *  it would end up being escaped when just rendering to static markup.
      *  This is, quote is literally "<p>Quote</p>", not the pure text itself.
      */
-    return renderToStaticMarkup(
-        <QuoteContainer>
-            <Quote>
-                {<div dangerouslySetInnerHTML={{ __html: quote }}/>}
-            </Quote>
-        </QuoteContainer>)
+    return renderToStaticMarkup(<QuoteContainer dangerouslySetInnerHTML={{ __html: quote }}/>)
 }
